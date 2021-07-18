39c392d671d489e1587a39dc377150d6f0378971
Catherine Smith <catsmith24601@gmail.com>
2021-07-18 08:23:13 -0400

    installed dependencies, implemented Apollo Server on server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+// Add the ApolloServer class 
+const { ApolloServer } = require('apollo-server-express');
+// Import the schemas directory, which contains the typeDef and resolver files
+const { typeDefs, resolvers } = require('./schemas');
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create a new instance of the ApolloServer class, which grants access to the core functionality required to implement graphql api.
+const server = new ApolloServer({
+  // Pass in the typeDefs and resolvers, which define the shape of the data as well as the functionality needed to retrieve it.
+  typeDefs,
+  resolvers,
+});
+
+// Apply ApolloServer to the Express server as middleware
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +30,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 });
